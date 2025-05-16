d81d5c5f44f0895e01c152bb8c6a500b21b234ad
Andriy <lerd.kros@gmail.com>
2025-05-16 09:06:29 +0300

    lab5: created Product, Category, User models with admin and fixtures

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+# Таблиця категорій
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+# Таблиця розмірів (для зв'язку багато-до-багатьох)
+class Size(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+# Таблиця товарів
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sizes = models.ManyToManyField(Size)
+    country = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+# Таблиця користувачів (проста, кастомна)
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
